913e9241ce599146560aa99df6fbf8c7ce4cda90
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-07-21 17:49:21 -0300

    Novo componente 'Slider.jsx'
    Foi criado um slider utilizando bootstrap

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+import { Slider } from "../components/Slider/Slider";
 
 export const Home = () => {
     return (
         <div>
+            <Slider />
             <h2>Hello this is the home page</h2>
             <button className="btn btn-secondary btn-sm">
                 bootstrap button
