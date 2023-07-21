a5901cee01caf86c4afd57d7d5abaad854c4f853
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-07-21 17:37:56 -0300

    Novo componente 'Navbar.jsx'
    Foi criado o menu de navegação

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ export const Navbar = () => {
                     className="collapse navbar-collapse"
                     id="navbarSupportedContent"
                 >
-                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <ul className="navbar-nav ms-auto gap-3">
                         <li className="nav-item">
                             <Link
                                 className="nav-link active"
