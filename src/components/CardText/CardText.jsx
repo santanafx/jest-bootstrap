59f4e58efb633e493a9c2cfd711b8a2f8165b4e8
Lucas Santana Figueiredo <santanafx@hotmail.com>
2023-07-21 19:31:28 -0300

    Novo componente 'CardText.jsx'
    Foi criado 3 cards na página Home

---
+++ src/components/CardText/CardText.jsx
@@ -0,0 +1,32 @@
+export const CardText = () => {
+    return (
+        <section className="f2f2f2 py-5">
+            <div className="container">
+                <div className="row">
+                    <div className="col-md-6">
+                        <h4>Our Vision</h4>
+                        <div className="sub-underline mb-3"></div>
+                        <p>
+                            Lorem ipsum dolor sit amet, consectetur adipisicing
+                            elit. Placeat dignissimos facere harum ipsa dolores
+                            saepe possimus repudiandae consequatur! At quasi
+                            harum nihil cupiditate facilis sequi quo ducimus
+                            corrupti repudiandae cumque!
+                        </p>
+                    </div>
+                    <div className="col-md-6">
+                        <h4>Our Mission</h4>
+                        <div className="sub-underline mb-3"></div>
+                        <p>
+                            Lorem ipsum, dolor sit amet consectetur adipisicing
+                            elit. Aspernatur, perferendis saepe sit nam placeat
+                            laborum tempore debitis reprehenderit libero eius
+                            dolores nulla facilis. Beatae consequuntur error rem
+                            reprehenderit in eveniet.
+                        </p>
+                    </div>
+                </div>
+            </div>
+        </section>
+    );
+};
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,89 @@
+import { CardText } from "../components/CardText/CardText";
 import { Slider } from "../components/Slider/Slider";
 
 export const Home = () => {
     return (
-        <div>
+        <main>
             <Slider />
-            <h2>Hello this is the home page</h2>
-            <button className="btn btn-secondary btn-sm">
-                bootstrap button
-            </button>
-        </div>
+            <section>
+                <div className="container py-4">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h4>Our Company</h4>
+                            <div className="underline mb-3"></div>
+                            <p>
+                                Lorem ipsum dolor sit amet consectetur
+                                adipisicing elit. Animi non culpa vero molestias
+                                unde illo laborum ad, dolor accusamus
+                                distinctio! Impedit excepturi provident amet
+                                nobis quisquam laudantium accusantium error
+                                quis.
+                            </p>
+                            <p>
+                                Lorem ipsum dolor sit, amet consectetur
+                                adipisicing elit. Vel molestiae explicabo facere
+                                quis accusantium, consequuntur nisi sit commodi
+                                consequatur odio quisquam ad, est aperiam,
+                                praesentium aut itaque asperiores tempora
+                                dolores!
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <CardText />
+            <section className="py-5">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-12">
+                            <h4>Our Services</h4>
+                            <div className="mb-3 underline"></div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="card shadow">
+                                <div className="card-body">
+                                    <h4>Web Designing</h4>
+                                    <div className="mb-3 sub-underline"></div>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipisicing elit. Ducimus quisquam quae
+                                        itaque voluptatibus error maxime.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="card shadow">
+                                <div className="card-body">
+                                    <h4>Development</h4>
+                                    <div className="mb-3 sub-underline"></div>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipisicing elit. Ducimus quisquam quae
+                                        itaque voluptatibus error maxime.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div className="col-md-4">
+                            <div className="card shadow">
+                                <div className="card-body">
+                                    <h4>Digital Marketing</h4>
+                                    <div className="mb-3 sub-underline"></div>
+                                    <p>
+                                        Lorem ipsum dolor sit amet, consectetur
+                                        adipisicing elit. Ducimus quisquam quae
+                                        itaque voluptatibus error maxime.
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </main>
     );
 };
